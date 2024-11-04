0842397fa71e2330abd9b9c2fc885df591c3f830
Parth Sinha <sinhaparth555@gmail.com>
2024-11-04 00:00:40 +0000

    added middleware security

---
+++ healthmate/app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     *
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('X-Frame-Options', 'DENY');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->headers->set('Referrer-Policy', 'no-referrer');
+        $response->headers->set('X-Developed-By', 'Parth Sinha');
+        return $response;
+    }
+}
